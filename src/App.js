a732aa75753819d4dc344f58a81a75ed8f33be97
SketchDotDesign <SketchDotDesign@gmail.com>
2017-11-07 21:43:46 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return [
+      <h3 key={1}>Something awesome.. coming soon <i class="em em-sunglasses" /></h3>
+    ]
+  }
+}
+
+export default App;
